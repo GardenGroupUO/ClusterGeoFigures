dacb63a2f8a1d88e9d341fb5f570d95006c45cb8
geoffreyweal <geoffrey.weal@gmail.com>
2021-11-19 19:02:13 -0800

    update version of ClusterGeoFigures Program v1.0

---
+++ Examples/Run_ClusterGeoFigures.py
@@ -0,0 +1,7 @@
+from ClusterGeoFigures import ClusterGeoFigures_Program
+
+r_cut = 3.0
+elements = ['Cu','Pd']
+path_to_xyz_files = 'dft_mins'
+
+ClusterGeoFigures_Program(r_cut,elements,path_to_xyz_files)
